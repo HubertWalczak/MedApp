0b753321d33f9726654f6e4189b10b3b69338d2f
HubertW <huberttwalczak@gmail.com>
2022-02-14 15:00:09 +0100

    Added a table with doctor's data

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/doctors", to: "doctor#index"
+  get "/doctors/:id", to: "doctor#show"
 end
